cb3c2f1f49619c345a15f24bc0a5869f03f7fc01
Edmund Kwan <edmund@promenadesoftware.com>
2019-09-02 11:36:57 -0700

    Create basic api get function for trending gifs

---
+++ src/managers/APIManager.js
@@ -0,0 +1,24 @@
+let apiKey = "aXvyXMehTPDEv8CqiRnyhmVwbTwCaX0M";
+
+const getAPIEndPoint = async (baseEndpoint, params) => {
+  let paramKeys = Object.keys(params);
+  let paramsToUrl = "";
+  paramKeys.forEach((paramKey) => {
+    paramsToUrl = paramsToUrl + "&" + paramKey + "=" + params[paramKey];
+  });
+  let endpoint = baseEndpoint + "?api_key=" + apiKey + paramsToUrl;
+  let response = await fetch(endpoint);
+  let data = await response.json();
+  return data;
+};
+
+const getTrendingGifs = async (offset) => {
+  let baseEndpoint = "http://api.giphy.com/v1/gifs/trending";
+  let limit = 6;
+  let params = {"limit": limit, "offset": offset};
+  let results = await getAPIEndPoint(baseEndpoint, params);
+
+  return results;
+};
+
+export default getTrendingGifs;
