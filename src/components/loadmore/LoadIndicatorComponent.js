5206da541a891d66d70ad9ad38c88379ee758896
Edmund Kwan <edmund@promenadesoftware.com>
2019-09-02 19:06:45 -0700

    Add lint deps and config

---
+++ src/components/loadmore/LoadIndicatorComponent.js
@@ -0,0 +1,21 @@
+import { CircularProgress } from "@material-ui/core";
+import styled from "styled-components";
+import React from 'react';
+
+const PaddedCircularProgress = styled(CircularProgress)`
+  && {
+    padding: 120px;
+  }
+`;
+
+const LoadIndicatorComponent = () => {
+  return (
+    <PaddedCircularProgress
+      variant="indeterminate"
+      size={75}
+      thickness={7}
+    />
+  )
+};
+
+export default LoadIndicatorComponent;
